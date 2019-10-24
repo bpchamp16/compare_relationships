3a9fb13746f17fda27a79d440b0095ccf3fa7916
bpchamp16 <jda150792@gmail.com>
2019-10-24 23:35:16 +0300

    first example

---
+++ pors.py
@@ -0,0 +1,25 @@
+import instaloader
+
+# Get instance
+L = instaloader.Instaloader()
+USER = "sf_bp16"
+PROFILE = "shagunina"
+# Login or load session
+#L.login(USER, PASSWORD)        # (login)
+L.interactive_login(USER)      # (ask password on terminal)
+#L.load_session_from_file(USER) # (load session created w/
+                               #  `instaloader -l USERNAME`)
+
+# Obtain profile metadata
+profile = instaloader.Profile.from_username(L.context, PROFILE)
+# Print list of followees
+followers = []
+for followee in profile.get_followees():
+     followers.append(followee.username)
+#    print("written\n")
+followers.sort()
+file = open(f"{PROFILE}.txt","a+")
+for i in followers:
+    file.write(i + '\n')
+file.close()
+print("numbers of followees  = ")
