515dec06db6d016678b924380dee599bd1510cb3
bpchamp16 <jda150792@gmail.com>
2019-10-25 13:17:00 +0300

    on the way to my own module

--- pors.py
+++ pors.py
@@ -1,25 +1,54 @@
 import instaloader
+import os
+import os.path
 
-# Get instance
+
+OUTPUT_RESULT = "/output"
+USER = ""
+PROFILE = ""
+
+
+# function to create output dir
+def createOutputDir():
+    path = os.getcwd()
+    path = path + OUTPUT_RESULT
+
+    try:
+        os.mkdir(path)
+    except OSError:
+        print("Creation of the directory %s failed" % path)
+    else:
+        print("Succesfully created the directory %s" % path)
+
+# function to create ouput file
+def createOutputFile(list_name):
+    file = open(f"{path}/{name}.txt", "a+")
+    for i in list_name:
+        file.write(i + '\n')
+
+    file.close()
+
+# function to get list of all followings by profile name
+def getListFollowings(name):
+    profile = instaloader.Profile.from_username(L.context, name)
+    followings = []
+    for i in profile.get_followees():
+        followings.append(i.username)
+    followings.sort()
+    return followings
+
+def getListCompareUsers(name1, name2):
+    followings1 = getListFollowings(name1)
+    followings2 = getListFollowings(name2)
+
+###         MAIN LOGIC         ###
+if os.path.exists(path):
+    print("Output folder already exists")
+else:
+    createOutputDir()
+
 L = instaloader.Instaloader()
-USER = "self_user"
-PROFILE = "user"
-# Login or load session
-#L.login(USER, PASSWORD)        # (login)
-L.interactive_login(USER)      # (ask password on terminal)
-#L.load_session_from_file(USER) # (load session created w/
-                               #  `instaloader -l USERNAME`)
+L.interactive_login(USER) 
 
-# Obtain profile metadata
-profile = instaloader.Profile.from_username(L.context, PROFILE)
-# Print list of followees
-followers = []
-for followee in profile.get_followees():
-     followers.append(followee.username)
-#    print("written\n")
-followers.sort()
-file = open(f"{PROFILE}.txt","a+")
-for i in followers:
-    file.write(i + '\n')
-file.close()
-print("numbers of followees  = ")
+
+print("Successfully exited from writing.")
